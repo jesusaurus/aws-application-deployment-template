a674e4920457eb5921a4ccca7074bb7d44ec0b77
linglp <linglp@umich.edu>
2022-12-16 14:11:05 -0500

    update to use v0.1.5-beta

--- cdk/docker_fargate/docker_fargate_stack.py
+++ cdk/docker_fargate/docker_fargate_stack.py
@@ -37,6 +37,7 @@ def get_required_env(name: str) -> str:
 def create_id() -> str:
     return get_required_env(STACK_NAME_PREFIX)+ID_SUFFIX
 
+
 CONTAINER_ENV = "CONTAINER_ENV" # name of env passed from GitHub action
 ENV_NAME = "ENV"
 
@@ -69,6 +70,7 @@ def create_secret(scope: Construct, name: str) -> str:
     return ecs.Secret.from_secrets_manager(isecret)
     # see also: https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_ecs/Secret.html
     # see also: ecs.Secret.from_ssm_parameter(ssm.IParameter(parameter_name=name))
+
 def get_hosted_zone_name() -> str:
 	return os.getenv(HOSTED_ZONE_NAME)
 	
@@ -89,9 +91,7 @@ class DockerFargateStack(Stack):
         cluster = ecs.Cluster(self, get_cluster_name(), vpc=vpc, container_insights=True)
 
         secrets = {
-        	# uncomment the following to add a secret as an environment variable
-        	# SECRETS_MANAGER_ENV_NAME: create_secret(self, get_secret_name())
-            SECRETS_MANAGER_ENV_NAME: create_secret(self, get_secret_name())
+        	SECRETS_MANAGER_ENV_NAME: create_secret(self, get_secret_name())
         }
 
         env_vars = {}
@@ -104,9 +104,10 @@ class DockerFargateStack(Stack):
     	    	   environment=env_vars,
     	    	   secrets = secrets,
     	    	   container_port = get_port())
-
+        
         zone = r53.PublicHostedZone.from_public_hosted_zone_attributes(self, id=stack_id+"_zone", hosted_zone_id=get_hosted_zone_id(), zone_name=get_hosted_zone_name())
 
+ 
         #
         # for options to pass to ApplicationLoadBalancedTaskImageOptions see:
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_ecs_patterns/ApplicationLoadBalancedTaskImageOptions.html#aws_cdk.aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions
@@ -118,14 +119,11 @@ class DockerFargateStack(Stack):
             desired_count=1,            # Number of copies of the 'task' (i.e. the app') running behind the ALB
             task_image_options=task_image_options,
             memory_limit_mib=1024,      # Default is 512
-            public_load_balancer=True, # Default is False
-            domain_name = get_host_name(), #The domain name for the service, e.g. “api.example.com.” )
-            domain_zone = zone)
-            # TODO TLS
-            #protocol=elbv2.ApplicationProtocol.HTTPS,
-            #domain_name="TBD", # The domain name for the service, e.g. “api.example.com.”
-            #domain_zone="TBD") #  The Route53 hosted zone for the domain, e.g. “example.com.”
-            
+            public_load_balancer=True,  # Default is False
+            # TLS:
+            protocol=elbv2.ApplicationProtocol.HTTPS,
+            domain_name=get_host_name(), # The domain name for the service, e.g. “api.example.com.”
+            domain_zone=zone) #  The Route53 hosted zone for the domain, e.g. “example.com.”            
             
         # Overriding health check timeout helps with sluggishly responding app's (e.g. Shiny)
         # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_elasticloadbalancingv2/ApplicationTargetGroup.html#aws_cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup    
